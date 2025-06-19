1f0a40855abae7756ab7dbe633b23d4666c4329a
yunish122 <manandharunish1@gmail.com>
2025-06-19 11:34:14 +0545

    yunish

--- logic.js
+++ logic.js
@@ -66,14 +66,14 @@ function toggleTurn(){
 }
 
 const winnerPattern = [
-    [0,1,2], // top row
-    [3,4,5], // middle row
-    [6,7,8], // bottom row
-    [0,3,6], // left column
-    [1,4,7], // middle column
-    [2,5,8], // right column
-    [0,4,8], // diagonal
-    [2,4,6]  // diagonal
+    [0,1,2], 
+    [3,4,5], 
+    [6,7,8], 
+    [0,3,6], 
+    [1,4,7], 
+    [2,5,8], 
+    [0,4,8], 
+    [2,4,6]  
 ];
 
 function check_winner(){
